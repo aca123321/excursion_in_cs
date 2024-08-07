fd3e1b5501af69c391789f3ff450f508fcea426b
yaseen-oakrev-gitlab <yaseen@oakrev.com>
2024-08-07 10:27:27 +0530

    waiting and exit status

--- unix_fork.c
+++ unix_fork.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  pid_t pid;
+  pid_t child_pid;
 
-  int n = 10;
-  pid = fork();
+  child_pid = fork();
+  int status = 1;
 
-  if (pid < 0) {  // error
+  if (child_pid < 0) {  // error
     fprintf(stderr, "Failed to create the child process\n");
-  } else if (pid == 0) {  // child process
-    fprintf(stdout, "Child process...\n");
-    return 0;
+  } else if (child_pid == 0) {  // child process
+    fprintf(stdout, "Child process with pid: %d and parent process pid: %d\n", getpid(), getppid());
+    exit(2);
   } else {  // parent process
-    wait(NULL);
-    fprintf(stdout, "Parent process...\n");
+    wait(&status);
+    fprintf(stdout, "Parent process with pid: %d waited on child with pid: %d and status: %d\n", getpid(), child_pid, WEXITSTATUS(status));
   }
   return 0;
 }
